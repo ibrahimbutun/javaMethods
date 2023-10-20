fc3a13bfde58bd9d99331f2b2b287b3a4d102381
ibrahimbutun <ibrahimbutun@outlook.com>
2023-10-20 15:34:58 +0300

    palindrom

---
+++ isPalindrom.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class isPalindrom {
+    static boolean isPalindrom(int number) {
+        int temp = number, reverseNumber = 0, lastNumber;
+
+        while (temp != 0) {
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            temp /= 10;
+        }
+
+        if (number == reverseNumber) {
+            System.out.println("palindrom");
+        }
+
+        else {
+            System.out.println("not palindrom");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("sayi giriniz: ");
+        int input = scanner.nextInt();
+        isPalindrom(input);
+    }
+}
